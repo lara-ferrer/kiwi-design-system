747cfe13b58f050e4bddd43cd9fb73777dc4bb08
lara-ferrer <larafpics@gmail.com>
2023-03-06 20:56:24 +0100

    [feat]: add default card styles

--- src/stories/components/organisms/card/Card.jsx
+++ src/stories/components/organisms/card/Card.jsx
@@ -2,21 +2,26 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./card.scss";
 import "../../../../styles/index.scss";
+import { Tag } from "../../atoms/tag/Tag";
 
-export const Card = ({ image, title, address, openTime, isOpen }) => {
+export const Card = ({ image, title, address, openTime, isOpen, categories }) => {
   return (
-    <div class="kiwi-card">
-      <img class="kiwi-card__image" src={image} alt={title} />
-      <div class="kiwi-card__content">
+    <div className="kiwi-card">
+      <img className="kiwi-card__image" src={image} alt={title} />
+      <div className="kiwi-card__content">
         <div>
-          <div class="pb--12">
+          <div className="pb--12">
             <h4>{title}</h4>
-            <p class="kiwi-subtitle">{address}</p>
+            <p className="kiwi-subtitle">{address}</p>
           </div>
-          <p class="kiwi-subtitle">{openTime}</p>
-          <p class="kiwi-subtitle kiwi-subtitle--alert">{isOpen && "NOW OPEN"}</p>
+          <div className="kiwi-card__categories">
+            {categories.map((category, i) => <Tag title={category} key={`kiwi-tag-${i}`} size="small" />)}
+          </div>
         </div>
-        <p class="kiwi-subtitle">3,2 OUT OF 1500 reviews</p>
+        <div className="kiwi-card__footer">
+          <p className="kiwi-subtitle">{openTime}</p>
+          <p className="kiwi-subtitle kiwi-subtitle--alert">{isOpen && "NOW OPEN"}</p>
+        </div>
       </div>
     </div>
   );
@@ -28,6 +33,7 @@ Card.propTypes = {
   address: PropTypes.string.isRequired,
   openTime: PropTypes.string.isRequired,
   isOpen: PropTypes.bool,
+  categories: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 Card.defaultProps = {
@@ -35,4 +41,5 @@ Card.defaultProps = {
   address: "Lettuce Street, 8, Zaragoza",
   openTime: "mon - sun: 13:00h - 16:00h",
   isOpen: true,
+  categories: ['Vegan', 'Vegetarian']
 };
